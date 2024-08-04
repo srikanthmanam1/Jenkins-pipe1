d5c881b2a8e5600ae4d5e39636c313be7de671e0
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2024-08-04 01:53:15 -0500

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,4 +1,4 @@
 resource "aws_security_group" "allow_http_ssh" {
-  name        = "launch-wizard-13"
+  name        = "launch-wizard-16"
   description = "Allow Ingress rules to allow SSH and HTTP connections"
 }
